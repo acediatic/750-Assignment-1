3828ba1517dddded7412fd2c2c8bce0addfdbe6e
Acediatic <hello@adamsinclair.kiwi>
2022-04-02 12:10:44 +1300

    feat: adds rounded cards to home page

--- stronger/lib/home.dart
+++ stronger/lib/home.dart
@@ -6,10 +6,51 @@ class Home extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
+    return Scaffold(
       // Displays a transparent app bar with a heart icon on the left hand
       // side and a profile icon on the right hand side.
-      appBar: StrongerAppBar(),
+      appBar: const StrongerAppBar(),
+      body: Center(
+        // list of rounded cards with titles "My Workouts", "Freedom Mode", and "Create Workout".
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: const [
+            RoundedCard(
+              text: 'My Workouts',
+            ),
+            RoundedCard(
+              text: 'Freedom Mode',
+            ),
+            RoundedCard(
+              text: 'Create Workout',
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class RoundedCard extends StatelessWidget {
+  final String text;
+  const RoundedCard({Key? key, required this.text}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      child: InkWell(
+        splashColor: Colors.blue.withAlpha(30),
+        onTap: () {
+          ;
+        },
+        child: SizedBox(
+          width: 300,
+          height: 100,
+          child: Center(
+            child: Text(text),
+          ),
+        ),
+      ),
     );
   }
 }
