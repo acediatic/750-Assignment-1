6cc12ab9e91e7adc58a03a435ac9425657f9249a
Acediatic <hello@adamsinclair.kiwi>
2022-04-06 22:08:21 +1200

    style: adds comma to workout page

--- stronger/lib/pages/workout_page.dart
+++ stronger/lib/pages/workout_page.dart
@@ -15,85 +15,84 @@ class WorkoutPage extends StatelessWidget {
     final workout = ModalRoute.of(context)!.settings.arguments as Workout;
 
     return StandardScaffold(
-        child: Center(
-      // creates two columns at the top, with a full width list at the bottom
-
-      child: Column(
-        children: [
-          Row(
-            crossAxisAlignment: CrossAxisAlignment.center,
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Expanded(
-                child: Column(
-                  mainAxisAlignment: MainAxisAlignment.start,
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Text(
-                      workout.title,
-                      style: Theme.of(context).textTheme.headline5,
-                    ),
-                    const SizedBox(
-                      height: 10,
-                    ),
-                    Text(
-                      workout.description,
-                      style: Theme.of(context).textTheme.bodyText1,
-                    ),
-                  ],
+      child: Center(
+        child: Column(
+          children: [
+            Row(
+              crossAxisAlignment: CrossAxisAlignment.center,
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Expanded(
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.start,
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(
+                        workout.title,
+                        style: Theme.of(context).textTheme.headline5,
+                      ),
+                      const SizedBox(
+                        height: 10,
+                      ),
+                      Text(
+                        workout.description,
+                        style: Theme.of(context).textTheme.bodyText1,
+                      ),
+                    ],
+                  ),
                 ),
+                Hero(child: FaIcon(workout.icon, size: 50), tag: workout),
+              ],
+            ),
+            const SizedBox(
+              height: 100,
+            ),
+            Text(
+              "Exercises",
+              style: Theme.of(context).textTheme.headline5,
+            ),
+            const SizedBox(
+              height: 16,
+            ),
+            Expanded(
+              child: ListView.builder(
+                itemCount: workout.exercises.length,
+                itemBuilder: (context, index) {
+                  final exercise = workout.exercises[index];
+                  return ListTile(
+                    leading: Text((index + 1).toString(),
+                        style: Theme.of(context).textTheme.headline6?.copyWith(
+                            color: Theme.of(context).colorScheme.primary)),
+                    title: Text(exercise.title),
+                    subtitle: Text(exercise.description),
+                    trailing: const Icon(Icons.keyboard_arrow_right),
+                    onTap: () {
+                      Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                            builder: (context) => ChangeNotifierProvider.value(
+                              child: const ExercisePage(),
+                              value: exercise,
+                            ),
+                          ));
+                    },
+                  );
+                },
               ),
-              Hero(child: FaIcon(workout.icon, size: 50), tag: workout),
-            ],
-          ),
-          const SizedBox(
-            height: 100,
-          ),
-          Text(
-            "Exercises",
-            style: Theme.of(context).textTheme.headline5,
-          ),
-          const SizedBox(
-            height: 16,
-          ),
-          Expanded(
-            child: ListView.builder(
-              itemCount: workout.exercises.length,
-              itemBuilder: (context, index) {
-                final exercise = workout.exercises[index];
-                return ListTile(
-                  leading: Text((index + 1).toString(),
-                      style: Theme.of(context).textTheme.headline6?.copyWith(
-                          color: Theme.of(context).colorScheme.primary)),
-                  title: Text(exercise.title),
-                  subtitle: Text(exercise.description),
-                  trailing: const Icon(Icons.keyboard_arrow_right),
-                  onTap: () {
-                    Navigator.push(
-                        context,
-                        MaterialPageRoute(
-                          builder: (context) => ChangeNotifierProvider.value(
-                            child: const ExercisePage(),
-                            value: exercise,
-                          ),
-                        ));
-                  },
-                );
-              },
             ),
-          ),
-          // complete workout button
-          Align(
-            alignment: Alignment.bottomRight,
-            child: FloatingActionButton(
-              onPressed: () {
-                Navigator.of(context).pop();
-              },
-              child: const Icon(Icons.home),
+            // complete workout button
+            Align(
+              alignment: Alignment.bottomRight,
+              child: FloatingActionButton(
+                onPressed: () {
+                  Navigator.of(context).pop();
+                },
+                child: const Icon(Icons.home),
+              ),
             ),
-          ),
-        ],
+          ],
+        ),
       ),
-    ));
+    );
   }
 }
