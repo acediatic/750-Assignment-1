1c180c0956443ecd09f914a1e3125360171d8f6a
Acediatic <hello@adamsinclair.kiwi>
2022-04-06 12:05:34 +1200

    refactor: create workout as consumer

--- stronger/lib/pages/create_workout_page.dart
+++ stronger/lib/pages/create_workout_page.dart
@@ -1,78 +1,69 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
-import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:provider/provider.dart';
 
 import '../components/standard_scaffold.dart';
-import '../model/exercise.dart';
+import '../model/workout.dart';
 
-class CreateWorkoutPage extends StatefulWidget {
+class CreateWorkoutPage extends StatelessWidget {
   const CreateWorkoutPage({Key? key}) : super(key: key);
 
   static const routeName = "/create_workout";
 
   @override
-  State<CreateWorkoutPage> createState() => _CreateWorkoutPageState();
-}
-
-class _CreateWorkoutPageState extends State<CreateWorkoutPage> {
-  String title = '';
-  String description = '';
-  IconData icon = FontAwesomeIcons.dumbbell;
-  List<Exercise> exercises = const [];
-  int targetReps = 0;
-
-  @override
   Widget build(BuildContext context) {
     return (StandardScaffold(
-      child: Column(
-        children: [
-          Text(
-            "Create Workout",
-            style: Theme.of(context).textTheme.headline6,
-          ),
-          const SizedBox(height: 16.0),
-          TextField(
-            decoration: const InputDecoration(
-              labelText: "Workout Name",
+      child: Consumer<Workout>(
+        builder: (context, workout, child) => Column(
+          children: [
+            Text(
+              "Create Workout",
+              style: Theme.of(context).textTheme.headline6,
             ),
-            onChanged: (value) => title = value,
-          ),
-          const SizedBox(height: 16.0),
-          TextField(
-            decoration: const InputDecoration(
-              labelText: "Description",
+            const SizedBox(height: 16.0),
+            TextField(
+              decoration: const InputDecoration(
+                labelText: "Workout Name",
+              ),
+              onChanged: (value) => workout.title = value,
             ),
-            onChanged: (value) => description = value,
-          ),
-          const SizedBox(height: 16.0),
-          TextField(
-            keyboardType: TextInputType.number,
-            inputFormatters: <TextInputFormatter>[
-              FilteringTextInputFormatter.digitsOnly
-            ], // Only numbers can be entered
-            decoration: const InputDecoration(
-              labelText: "Target Reps",
+            const SizedBox(height: 16.0),
+            TextField(
+              decoration: const InputDecoration(
+                labelText: "Description",
+              ),
+              onChanged: (value) => workout.description = value,
             ),
-            onChanged: (value) => targetReps = value as int,
-          ),
-          const SizedBox(height: 16.0),
-          // button to navigate to the add exercises screen
-          ElevatedButton(
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: const [
-                Spacer(flex: 5),
-                Text("Add Exercises"),
-                Spacer(flex: 1),
-                Icon(Icons.arrow_forward),
-                Spacer(flex: 5),
-              ],
+            const SizedBox(height: 16.0),
+            TextField(
+              keyboardType: TextInputType.number,
+              inputFormatters: <TextInputFormatter>[
+                FilteringTextInputFormatter.digitsOnly
+              ], // Only numbers can be entered
+              decoration: const InputDecoration(
+                labelText: "Target Rounds",
+              ),
+              onChanged: (value) => workout.targetRounds = value as int,
             ),
-            onPressed: () {
-              // TODO navigate to add exercises screen
-            },
-          ),
-        ],
+            const SizedBox(height: 16.0),
+            // button to navigate to the add exercises screen
+            ElevatedButton(
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: const [
+                  Spacer(flex: 5),
+                  Text("Add Exercises"),
+                  Spacer(flex: 1),
+                  Icon(Icons.arrow_forward),
+                  Spacer(flex: 5),
+                ],
+              ),
+              onPressed: () {
+                // TODO navigate to add exercises page
+              },
+            ),
+          ],
+        ),
       ),
     ));
   }
