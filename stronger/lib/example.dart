d447eab0949fb5971dff56aca841ceb4c243767c
Acediatic <hello@adamsinclair.kiwi>
2022-04-06 21:08:34 +1200

    refactor: moves demo workouts to separate file

---
+++ stronger/lib/example.dart
@@ -0,0 +1,64 @@
+import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import './model/workout.dart';
+import './model/exercise.dart';
+
+List<Workout> demoWorkouts = [
+  Workout.withExercises(
+    title: "Chest",
+    icon: FontAwesomeIcons.childReaching,
+    description: "Chest workout",
+    exercises: [
+      Exercise(
+        title: "Bench press",
+        icon: Icons.access_time,
+        description: "Bench press",
+        targetReps: 10,
+      ),
+      Exercise(
+        title: "Incline bench press",
+        icon: Icons.access_time,
+        description: "Incline bench press",
+        targetReps: 10,
+      ),
+    ],
+  ),
+  Workout.withExercises(
+    title: "Full Body",
+    icon: FontAwesomeIcons.personRunning,
+    description: "Upper Body workout",
+    exercises: [
+      Exercise(
+        title: "Deadlift",
+        icon: Icons.access_time,
+        description: "Deadlift",
+        targetReps: 10,
+      ),
+      Exercise(
+        title: "Pull up",
+        icon: Icons.access_time,
+        description: "Pull up",
+        targetReps: 10,
+      ),
+    ],
+  ),
+  Workout.withExercises(
+    title: "Legs",
+    icon: FontAwesomeIcons.socks,
+    description: "Legs workout",
+    exercises: [
+      Exercise(
+        title: "Squat",
+        icon: Icons.access_time,
+        description: "Squat",
+        targetReps: 10,
+      ),
+      Exercise(
+        title: "Leg press",
+        icon: Icons.access_time,
+        description: "Leg press",
+        targetReps: 10,
+      ),
+    ],
+  ),
+];
--- stronger/lib/main.dart
+++ stronger/lib/main.dart
@@ -1,82 +1,19 @@
 import 'package:flutter/material.dart';
+import 'package:stronger/example.dart';
+import 'package:stronger/model/workouts_list.dart';
 import 'package:stronger/pages/create_workout_page.dart';
 import 'package:stronger/pages/exercise_page.dart';
-import 'package:stronger/model/workouts_list.dart';
 import 'package:stronger/pages/freedom_mode_page.dart';
 import 'package:stronger/pages/workout_page.dart';
 import 'pages/add_exercise_page.dart';
 import 'pages/home.dart';
 import 'package:provider/provider.dart';
 import "package:stronger/model/workout.dart";
-import "package:stronger/model/exercise.dart";
 import 'pages/list_workouts.dart';
-import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
 void main() {
   runApp(ChangeNotifierProvider(
-    child: const MyApp(),
-    create: (context) => WorkoutsList(
-      [
-        Workout.withExercises(
-          title: "Chest",
-          icon: FontAwesomeIcons.childReaching,
-          description: "Chest workout",
-          exercises: [
-            Exercise(
-              title: "Bench press",
-              icon: Icons.access_time,
-              description: "Bench press",
-              targetReps: 10,
-            ),
-            Exercise(
-              title: "Incline bench press",
-              icon: Icons.access_time,
-              description: "Incline bench press",
-              targetReps: 10,
-            ),
-          ],
-        ),
-        Workout.withExercises(
-          title: "Full Body",
-          icon: FontAwesomeIcons.personRunning,
-          description: "Upper Body workout",
-          exercises: [
-            Exercise(
-              title: "Deadlift",
-              icon: Icons.access_time,
-              description: "Deadlift",
-              targetReps: 10,
-            ),
-            Exercise(
-              title: "Pull up",
-              icon: Icons.access_time,
-              description: "Pull up",
-              targetReps: 10,
-            ),
-          ],
-        ),
-        Workout.withExercises(
-          title: "Legs",
-          icon: FontAwesomeIcons.socks,
-          description: "Legs workout",
-          exercises: [
-            Exercise(
-              title: "Squat",
-              icon: Icons.access_time,
-              description: "Squat",
-              targetReps: 10,
-            ),
-            Exercise(
-              title: "Leg press",
-              icon: Icons.access_time,
-              description: "Leg press",
-              targetReps: 10,
-            ),
-          ],
-        ),
-      ],
-    ),
-  ));
+      child: const MyApp(), create: (context) => WorkoutsList(demoWorkouts)));
 }
 
 const freedomModeRoute = '/freedom_mode';
